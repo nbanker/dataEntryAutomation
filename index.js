15320fec0f97e89043040b2e85ef1a673d2b63dc
cijin <cijininurhood@gmail.com>
2020-08-11 15:15:12 +0700

    made progress today :)

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const xlsx = require('xlsx');
-
-const provision  = xlsx.readFile('./data.xlsx');
-const sheets = provision.SheetNames;
-const rows = xlsx.utils.sheet_to_json(provision.Sheets[sheets[0]], {header: 1});
+const tableToJson = require('tabletojson').Tabletojson;
+const flatten = require('./flatten';)
+// const provision  = xlsx.readFile('./data.xlsx');
+// const sheets = provision.SheetNames;
+// const rows = xlsx.utils.sheet_to_json(provision.Sheets[sheets[0]], {header: 1});
 
 app.get('/', (req, res) => {
-  res.send(rows);
+  res.send(homeConsoles);
 });
 
 app.listen(process.env.PORT || 9001, () => {
